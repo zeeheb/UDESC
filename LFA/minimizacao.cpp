9360ec198cdb5ea83649a7b43c26f01c971fee89
zeeheb <zefutbrandao@gmail.com>
2018-05-28 13:58:14 -0300

    kd erro

--- LFA/minimizacao.cpp
+++ LFA/minimizacao.cpp
@@ -4,6 +4,9 @@
 #include <stack>
 #include <utility>
 
+#define MARCADO 1
+#define NAOMARCADO 0
+
 
 using namespace std;
 
@@ -16,27 +19,65 @@ int main(int argc, char const *argv[]) {
   vector<int> estNaoMarcados;
   vector<int> estFinais;
 
-  for(int i=0; i<qtEF; i++) {
+  for(int i=0; i<qtEF; i++) { // adicionando estados finais em vector
     int aux;
     cin >> aux;
     estFinais.push_back(aux);
   }
 
-  pair<char, pair<int,int> > tabela[20];
+
+  pair<char, pair<int,int> > tabelaTransicao[20]; // parear estados com alfabeto
 
   for(int i=0; i<nroEstados*tamAlfabeto; i++) {
     int aux, aux2;
     char auxchar;
     cin >> aux >> auxchar >> aux2;
-      tabela[i] = make_pair(auxchar, make_pair(aux, aux2));
+      tabelaTransicao[i] = make_pair(auxchar, make_pair(aux, aux2));
   }
+
+
+vector<int> estados;
+  for(int i=0; i<nroEstados; i++) {
+    estados.push_back(i);
+  }
+
+
+  pair<int, pair<int, int> > tabelaMini[20]; // tabela para minimizar, ex: par<MARCADO, (1, 0)>
+
+  for(int i=0; i<nroEstados; i++) {
+    for(int j=0;j<nroEstados; j++) {
+      if(((i != j) || (i < j)) && (i < j) ) { //caso iguais ou parte de cima da matriz, break, para nao duplicar tabela
+          cout << estados[i] << " e " << estados[j] << "\n ";
+          vector<int>::iterator it;
+           it = find(estFinais.begin(), estFinais.end(), estados[i]);
+            if(it != estFinais.end()) { //encontrou o estado em estados finais
+               vector<int>::iterator it2;
+                it2 = find(estFinais.begin(), estFinais.end(), estados[j]);
+                if( it2 == estFinais.end()) { //e nao achou o segundo estado como final
+                  tabelaMini[i] = make_pair(MARCADO, make_pair(estados[i], estados[j]));
+              } else {
+                tabelaMini[i] = make_pair(NAOMARCADO, make_pair(estados[i], estados[j]));
+              }
+          } 
+      }
+    }
+  }
+
+
+
+
 
 // ======================== DEBUG ==============================
 // =============================================================
-  cout << "Transições: \n";
+  cout << "\nEstados: ";
+    for(int i=0; i<nroEstados; i++){
+      cout << estados[i] << " " ;
+    }
+
+  cout << "\n\nTransições: \n";
     for(int i=0; i<nroEstados*tamAlfabeto; i++) {
-      cout << "Estado " << tabela[i].second.first << " lendo " << tabela[i].first
-      << " = " << tabela[i].second.second << endl;
+      cout << "Estado " << tabelaTransicao[i].second.first << " lendo " << tabelaTransicao[i].first
+      << " = " << tabelaTransicao[i].second.second << endl;
     }
 
   cout << "Estados finais: ";
@@ -44,7 +85,13 @@ int main(int argc, char const *argv[]) {
       cout << estFinais[i] << " ";
     }
 
-  cout << "\nEstado inicial: " << estInicial << endl;
+  cout << "\n\nEstados com X: \n";
+    for(int i=0; i<nroEstados*3; i++) {
+      cout << tabelaMini[i].second.first << " com " << tabelaMini[i].second.second <<
+      " = " << tabelaMini[i].first << endl;
+    }
+
+  cout << "\nEstado inicial: " << estInicial << endl << endl;
 // ==============================================================
 // ==============================================================
 
