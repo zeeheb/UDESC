65d5277168720fcdaa662adc1d9e36e45ab0437a
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2019-03-25 14:25:50 -0300

    mult 0 inducão

--- MFO/lf/Induction.v
+++ MFO/lf/Induction.v
@@ -197,7 +197,9 @@ Proof.
 Theorem mult_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity. Qed. 
 
 Theorem plus_n_Sm : forall n m : nat,
   S (n + m) = n + (S m).
