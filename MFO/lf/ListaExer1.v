594f293acfcf29aa40a88fab0a46e2bdd6a44216
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2019-04-22 14:55:18 -0300

    lista 1 - metade feito

---
+++ MFO/lf/ListaExer1.v
@@ -0,0 +1,69 @@
+(** * Métodos Formais Lista de Exercícios 1 *)
+
+From LF Require Export Induction.
+
+(** Prove usando Coq que $\Sigma_{i=0}^{n}i = (n^2 + n)/2$ ([sum_n]). 
+    No desenvolvimento desse exercício serão necessárias algumas definições 
+    feitas nos módulos [Basics.v] e [Induction.v] são elas: [plus_n_O], 
+    [plus_assoc], [plus_comm], [mult_comm] e [mult_S]. *)
+
+(** A função somatório de números naturais de 0 até $n$ pode ser implementada 
+    como: *)
+
+Fixpoint sum (n : nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n + sum n'
+  end.
+
+(** Com objetivo de simplificar as provas a função [div2] implementa a 
+    divisão por 2. *) 
+
+Fixpoint div2 (n:nat) : nat :=
+  match n with
+  | O => O
+  | S O => O 
+  | S (S n') => S (div2 n')  
+end.  
+
+
+Theorem plus_n_1 : forall (n : nat),
+  n + 1 = S (n).
+Proof. Admitted.
+
+
+Theorem plus_n_Sm : forall (n m:nat),
+  n + S m = S (n + m).
+Proof.
+  Admitted.
+
+Theorem mult_2_n_plus : forall (n : nat),
+  n + n = 2 * n.
+Proof.
+  Admitted.
+
+Theorem mult2_div2 : forall n : nat,
+  n = div2 (2 * n).
+Proof.
+  Admitted.
+
+Theorem div2_mult2_plus: forall (n m : nat),
+  n + div2 m = div2 (2 * n + m).
+Proof.
+  Admitted.
+
+Theorem mult_Sn_m : forall (n m : nat),
+  S n * m = m + n * m.
+Proof.
+  Admitted.
+
+Theorem sum_Sn : forall n : nat,
+  sum (S n) = S n + sum n.
+Proof.
+  Admitted.
+
+Theorem sum_n : forall n : nat,
+  sum n = div2 (n * (n + 1)).
+Proof.
+  Admitted. 
+
